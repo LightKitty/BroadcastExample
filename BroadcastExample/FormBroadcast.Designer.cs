64c67729742156eba0026146c0d6777021881630
wnmwi <383929371@qq.com>
2019-10-15 18:36:05 +0800

    第一次提交

---
+++ BroadcastExample/FormBroadcast.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace BroadcastExample
+{
+    partial class FormBroadcast
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(379, 21);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 25);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "发送";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(367, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(448, 244);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.buttonSend);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 59);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "输入发送的广播信息";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 77);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(460, 272);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "接收的广播信息";
+            // 
+            // FormBroadcast
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormBroadcast";
+            this.Text = "Udp广播";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormBroadcast_FormClosing);
+            this.Load += new System.EventHandler(this.FormBroadcast_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ BroadcastExample/FormBroadcast.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BroadcastExample
+{
+    public partial class FormBroadcast : Form
+    {
+        delegate void AppendStringCallback(string text);
+        AppendStringCallback appendStringCallback;
+        //使用的接收端口号
+        private int port = 8001;
+        private UdpClient udpClient;
+        public FormBroadcast()
+        {
+            InitializeComponent();
+            appendStringCallback = new AppendStringCallback(AppendString);
+        }
+        private void AppendString(string text)
+        {
+            if (richTextBox1.InvokeRequired == true)
+            {
+                this.Invoke(appendStringCallback, text);
+            }
+            else
+            {
+                richTextBox1.AppendText(text + "\r\n");
+            }
+        }
+        /// <summary>
+        /// 在后台运行的接收线程
+        /// </summary>
+        private void ReceiveData()
+        {
+            //在本机指定的端口接收
+            udpClient = new UdpClient(port);
+            IPEndPoint remote = null;
+            //接收从远程主机发送过来的信息；
+            while (true)
+            {
+                try
+                {
+                    //关闭udpClient 时此句会产生异常
+                    byte[] bytes = udpClient.Receive(ref remote);
+                    string str = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+                    AppendString(string.Format("来自{0}：{1}", remote, str));
+                }
+                catch
+                {
+                    //退出循环，结束线程
+                    break;
+                }
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            UdpClient myUdpClient = new UdpClient();
+            try
+            {
+                //让其自动提供子网中的IP 广播地址
+                IPEndPoint iep = new IPEndPoint(IPAddress.Broadcast, 8001);
+                //允许发送和接收广播数据报
+                myUdpClient.EnableBroadcast = true;
+                //将发送内容转换为字节数组
+                byte[] bytes = Encoding.UTF8.GetBytes(textBox1.Text);
+                //向子网发送信息
+                myUdpClient.Send(bytes, bytes.Length, iep);
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "发送失败");
+            }
+            finally
+            {
+                myUdpClient.Close();
+            }
+        }
+
+        private void FormBroadcast_Load(object sender, EventArgs e)
+        {
+            Thread myThread = new Thread(new ThreadStart(ReceiveData));
+            //将线程设为后台运行
+            myThread.IsBackground = true;
+            myThread.Start();
+        }
+
+        private void FormBroadcast_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            udpClient.Close();
+        }
+    }
+}
